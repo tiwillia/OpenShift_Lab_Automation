f1ba8e01961c294587dd639e06312cd1c26b44e2
Timothy Williams <tiwillia@redhat.com>
2014-08-05 15:41:41 -0400

    Added better logging

--- lib/deploymenthandler.rb
+++ lib/deploymenthandler.rb
@@ -1,6 +1,6 @@
 class DeploymentHandler
 
-# This is the handler that controls deployment fors and their respective threads.
+# This is the handler that controls deployments and their respective threads.
 # 'start' creates a single thread that watches for work
 #     work must be in the format {action => "start|stop|restart", :project => Project.obj}
 # That thread then spins up individual threads as work is added.
@@ -13,13 +13,13 @@ class DeploymentHandler
   # Add work to the queue.
   # work should be a hash like: {:action => "stop", :project => Project.obj}
   def enqueue(work)
-    Rails.logger.info "DEPLOYMENTS: Adding work to queue: #{work.inspect}."
+    dlog "DEPLOYMENTS: Adding work to queue: #{work.inspect}."
     @queue << work
     if not running?
-      Rails.logger.info "DEPLOYMENTS: Starting deployment handler thread..."
+      dlog "DEPLOYMENTS: Starting deployment handler thread..."
       start
     end
-    Rails.logger.debug "DEPLOYMENTS: #{@queue.inspect}"
+    dlog("DEPLOYMENTS: #{@queue.inspect}",:debug)
   end
  
   # Check if the head thread is running 
@@ -32,7 +32,7 @@ private
   # This is the loop we run through constantly in the background to watch for work.
   def start
     if running?
-      Rails.logger.error "DEPLOYMENTS: ERROR Tried to start running deployment handler."
+      dlog("DEPLOYMENTS: ERROR Tried to start running deployment handler.",:error)
       return false
     end
     @thread = Thread.new do
@@ -41,9 +41,11 @@ private
           sleep 10
         end
         work = @queue.pop
-        Rails.logger.info "DEPLOYMENTS: Work found in queue, starting..."
-        do_work(work)
-        Rails.logger.info "DEPLOYMENTS: Work completed."
+        dlog "DEPLOYMENTS: Work found in queue, starting..."
+        Thread.new do
+          do_work(work)
+          dlog "DEPLOYMENTS: Work completed."
+        end
       end
     end
   end
@@ -56,37 +58,37 @@ private
     case work[:action]
 
     when "start"
-      Rails.logger.info "DEPLOYMENTS: Starting deployment #{project.name}"
+      dlog "DEPLOYMENTS: Starting deployment #{project.name}"
       begin
-        Rails.logger.info "DEPLOYMENTS: Started deployment #{project.name}"
+        dlog "DEPLOYMENTS: Started deployment #{project.name}"
         begin_deployment(project)
       rescue => e
-        Rails.logger.error "DEPLOYMENTS: ERROR could not start deployment #{e.message}"
-        Rails.logger.error "DEPLOYMENTS: #{e.backtrace}"
+        dlog("DEPLOYMENTS: ERROR could not start deployment #{e.message}", :error)
+        dlog("DEPLOYMENTS: #{e.backtrace}", :error)
       end
 
     when "stop"
-      Rails.logger.info "DEPLOYMENTS: Stopping deployment #{project.name}"
+      dlog "DEPLOYMENTS: Stopping deployment #{project.name}"
       begin
         destroy_deployment(project)
-        Rails.logger.info "DEPLOYMENTS: Stopped deployment #{project.name}"
+        dlog "DEPLOYMENTS: Stopped deployment #{project.name}"
       rescue => e
-        Rails.logger.error "DEPLOYMENTS: ERROR could not stop deployment #{e.message}"
-        Rails.logger.error "DEPLOYMENTS: #{e.backtrace}"
+        dlog("DEPLOYMENTS: ERROR could not stop deployment #{e.message}"
+        dlog("DEPLOYMENTS: #{e.backtrace}"
       end
 
     when "restart"
-      Rails.logger.info "DEPLOYMENTS: Restarting deployment #{project.name}"
+      dlog "DEPLOYMENTS: Restarting deployment #{project.name}"
       begin
         restart_deployment(project)
-        Rails.logger.info "DEPLOYMENTS: Restarted deployment #{project.name}"
+        dlog "DEPLOYMENTS: Restarted deployment #{project.name}"
       rescue => e
-        Rails.logger.error "DEPLOYMENTS: ERROR could not restart deployment #{e.message}"
-        Rails.logger.error "DEPLOYMENTS: #{e.backtrace}"
+        dlog("DEPLOYMENTS: ERROR could not restart deployment #{e.message}"
+        dlog("DEPLOYMENTS: #{e.backtrace}"
       end
      
     else
-      Rails.logger.error "DEPLOYMENTS: Action not recognized" 
+      dlog("DEPLOYMENTS: Action not recognized" 
     end
   end
 
@@ -101,9 +103,9 @@ private
     phase3 = []  # All nodes
     phase4 = []  # The final mongodb and the brokers
  
-    Rails.logger.info "DEPLOYMENTS: Getting project details..." 
+    dlog "DEPLOYMENTS: Getting project details..." 
     project_details = project.details
-    Rails.logger.info "DEPLOYMENTS: Got project details: #{project_details.inspect.to_s}" 
+    dlog "DEPLOYMENTS: Got project details: #{project_details.inspect.to_s}" 
 
     datastore_instance = ""
 
@@ -127,22 +129,22 @@ private
       end
     end
 
-    Rails.logger.info "DEPLOYMENTS: Created phases, starting instances..." 
-    Rails.logger.info "DEPLOYMENTS: Phase1: #{phase1}" 
-    Rails.logger.info "DEPLOYMENTS: Phase2: #{phase2}" 
-    Rails.logger.info "DEPLOYMENTS: Phase3: #{phase3}" 
-    Rails.logger.info "DEPLOYMENTS: Phase4: #{phase4}" 
+    dlog "DEPLOYMENTS: Created phases, starting instances..." 
+    dlog "DEPLOYMENTS: Phase1: #{phase1}" 
+    dlog "DEPLOYMENTS: Phase2: #{phase2}" 
+    dlog "DEPLOYMENTS: Phase3: #{phase3}" 
+    dlog "DEPLOYMENTS: Phase4: #{phase4}" 
     phase1.each {|i| i.start }   
-    Rails.logger.info "DEPLOYMENTS: Phase one started, waiting 2 minutes..." 
+    dlog "DEPLOYMENTS: Phase one started, waiting 2 minutes..." 
     sleep 120 
-    Rails.logger.info "DEPLOYMENTS: Phase 2 + 3 begin..." 
+    dlog "DEPLOYMENTS: Phase 2 + 3 begin..." 
     phase2.each {|i| i.start}    
     phase3.each {|i| i.start}    
-    Rails.logger.info "DEPLOYMENTS: Phase 2 + 3 complete, waiting 2 minutes..." 
+    dlog "DEPLOYMENTS: Phase 2 + 3 complete, waiting 2 minutes..." 
     sleep 120
-    Rails.logger.info "DEPLOYMENTS: Phase 4 begin..." 
+    dlog "DEPLOYMENTS: Phase 4 begin..." 
     phase4.each {|i| i.start}    
-    Rails.logger.info "DEPLOYMENTS: Phase 4 complete, waiting for completion..." 
+    dlog "DEPLOYMENTS: Phase 4 complete, waiting for completion..." 
 
     sleep 30
 
@@ -163,7 +165,7 @@ private
           sleep 120 unless result == "DONE"
         end
         complete_instances << inst
-        Rails.logger.info "DEPLOYMENTS: #{inst.fqdn} complete!"
+        dlog "DEPLOYMENTS: #{inst.fqdn} complete!"
       end
       if complete_instances.count == project.instances.count
         all_complete = true
@@ -172,7 +174,7 @@ private
 
     # Datastore replicant configuration
     if datastore_instance.class == Instance
-      Rails.logger.info "DEPLOYMENTS: Configuring replica set..."
+      dlog "DEPLOYMENTS: Configuring replica set..."
       replicants_complete = false
       tries = 0
       until replicants_complete || tries > 5 do
@@ -184,20 +186,20 @@ private
         if exit_code == "0"
           replicants_complete = true
         else
-          Rails.logger.error "DEPLOYMENTS: Replica set configuration failed, trying again..."
+          dlog("DEPLOYMENTS: Replica set configuration failed, trying again...", :error)
           sleep 30
         end
       end
       if replicants_complete
-        Rails.logger.info "DEPLOYMENTS: Datastore replicas configured."
+        dlog "DEPLOYMENTS: Datastore replicas configured."
       else
-        Rails.logger.error "DEPLOYMENTS: Could not configure datstore after #{tries} tries. Giving up."
+        dlog("DEPLOYMENTS: Could not configure datstore after #{tries} tries. Giving up.", :error)
       end
     end
 
     # Post deployment script
     if project.ose_version =~ /2\.[1,2,3]/
-      Rails.logger.info "DEPLOYMENTS: Running post deploy..."
+      dlog "DEPLOYMENTS: Running post deploy..."
       post_deploy_complete = false
       tries = 0
       until post_deploy_complete || tries > 5 do
@@ -209,18 +211,18 @@ private
         if exit_code == "0"
           post_deploy_complete = true
         else
-          Rails.logger.error "DEPLOYMENTS: Post deployment failed, trying again..."
+          dlog("DEPLOYMENTS: Post deployment failed, trying again...", :error)
           sleep 30
         end
       end
       if post_deploy_complete
-        Rails.logger.info "DEPLOYMENTS: Post deployment complete."
+        dlog "DEPLOYMENTS: Post deployment complete."
       else
-        Rails.logger.error "DEPLOYMENTS: Could not post deploy after #{tries} tries. Giving up."
+        dlog("DEPLOYMENTS: Could not post deploy after #{tries} tries. Giving up.", :error)
       end
     end
     
-    Rails.logger.info "DEPLOYMENTS: Deployment complete!"
+    dlog "DEPLOYMENTS: Deployment complete!"
 
   end
 
@@ -244,17 +246,24 @@ private
     rescue => e
       tries += 1
       if tries < 6
-        Rails.logger.error "DEPLOYMENTS: SSH to #{instance.fqdn} failed on attempt ##{tries} with #{e.class}. Retrying..."
+        dlog("DEPLOYMENTS: SSH to #{instance.fqdn} failed on attempt ##{tries} with #{e.class}. Retrying...", :error)
         sleep 10
         retry
       else
-        Rails.logger.error "DEPLOYMENTS: Could not ssh to #{instance.fqdn} after #{tries.to_s} tries:"
-        Rails.logger.error "DEPLOYMENTS: #{e.class} #{e.message}"
-        Rails.logger.error "DEPLOYMENTS: #{e.backtrace}"
+        dlog("DEPLOYMENTS: Could not ssh to #{instance.fqdn} after #{tries.to_s} tries:", :error)
+        dlog("DEPLOYMENTS: #{e.class} #{e.message}", :error)
+        dlog("DEPLOYMENTS: #{e.backtrace}", :error)
         raise e
       end
     end
     return ssh
+  end
+
+  # Easy way to log from deployments
+  def dlog(message, level = :info)
+    date_time = DateTime.now.to_s
+    full_message = "[" + date_time + "] (DEPLOYMENT) " + message
+    Rails.logger.send level, full_message
   end
 
 end
