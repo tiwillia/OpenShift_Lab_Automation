34c24cb29baf315beacbe01f5a4b04fd8a057110
tiwillia@redhat.com <tiwillia@redhat.com>
2014-09-23 10:36:54 -0400

    Further fleshed out help page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,8 @@ class WelcomeController < ApplicationController
 
   def help
     @questions = {"What is the purpose of this application?" => "purpose",
+                  "None of the existing configurations work for my reproducer. Can I create my own environment?" => "my_own",
+                  "How long does a deployment take?" => "deployment_time",
                   "Where are the OpenShift instances hosted?" => "hosted",
                   "How do the automatic deployments work?" => "deployments",
                   "Why can I not see the root passwords of instances?" => "root_passwd",
